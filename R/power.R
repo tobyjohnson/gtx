809b046be1be1b6eea7ad6cc4aeca24109bec004
Toby Johnson <Toby.x.Johnson@gsk.com>
2015-12-11 11:38:31 +0000

    fupower added

--- R/power.R
+++ R/power.R
@@ -13,3 +13,56 @@ chi2power <- Vectorize(function(alpha, ncp, df = 1) {
   cv <- qchisq(alpha, df = df, lower.tail = FALSE) # critical value for chisq statistic
   return(pchisq(cv, df = df, lower.tail = FALSE, ncp = ncp))
 })
+
+fupower <- function(p0, n0, n1, beta0, invlog = FALSE, alpha = 0.05,
+                    plty = c("solid", "dashed", "dotdash"), elty = "dotted",
+                    pcol = "red", ecol = "blue") {
+  chi0 <- qchisq(p0, df = 1, lower.tail = FALSE) # discovery chi2 statistic
+
+  ## wlog assume discovery effect direction is positive
+  xvec <- seq(from = min(0, sqrt(chi0) - 3), to = sqrt(chi0) + 3, length.out = 100)
+  zcrit <- qnorm(alpha, lower.tail = FALSE) # for one-sided test
+
+  n1 <- sort(na.omit(n1), decreasing = TRUE)
+
+  plty <- rep(plty, length.out = length(n1))
+  names(plty) <- as.character(n1)
+  pcol <- rep(pcol, length.out = length(n1))
+  names(pcol) <- as.character(n1)
+  
+  plot.new()
+  plot.window(range(xvec), c(0, 1))
+  abline(h = pretty(0:1), col = "grey")
+  box()
+  axis(2, las = 1)
+  title(ylab = "Power")
+  if (!missing(beta0)) {
+    if (invlog) {
+      pp <- pretty(exp(xvec/sqrt(chi0)*beta0))
+      axis(1, at=log(pp)/beta0*sqrt(chi0), labels=pp)
+      title(xlab = "Effect size (log scale)")
+    } else {
+      pp <- pretty(xvec/sqrt(chi0)*beta0)
+      axis(1, at=pp/beta0*sqrt(chi0), labels=pp)
+      title(xlab = "Effect size")
+    }
+  }
+  
+  pe <- dnorm(xvec, mean = sqrt(chi0))*sqrt(2*pi)*0.3
+  lines(xvec, pe, lty = elty, col = ecol)
+  
+  ip <- sapply(n1, function(n) {
+    pow <- pnorm(zcrit, mean = xvec*sqrt(n/n0), lower.tail = FALSE)
+    lines(xvec, pow, lty = plty[[as.character(n)]], col = pcol[[as.character(n)]]) # side effect
+    return(sum(pow*pe)/sum(pe))
+  })
+
+  plot.new()
+  legend("topleft", bty = "o", 
+         lty = c(plty, elty),
+         col = c(pcol, ecol),
+         legend = c(paste("N =", n1, "integr. power =", round(ip, 2)),
+           "estimated effect size"))
+
+  return(invisible(NULL))
+}
