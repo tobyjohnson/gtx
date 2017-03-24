3845eb7285fbb17aab0b5710bf9d3803465ac5c1
Toby Johnson <Toby.x.Johnson@gsk.com>
2017-03-24 16:59:40 +0000

    Function for colocalisation analyses

--- R/abf.R
+++ R/abf.R
@@ -29,3 +29,28 @@ abf.t <- function(beta, se, priorscale, df = 1, gridrange = 3, griddensity = 20)
     sum(dxx * dt(xx/priorscale, df = df)/priorscale * dnorm(beta[idx], xx, se[idx]))/dnorm(beta[idx], 0, se[idx])
   }))
 }
+
+## Colocalisation analysis, implementing method of Giambartolomei et al. 2014
+coloc.fast <- function(data, rounded = 6,
+                      priorsd1 = 1, priorsd2 = 1, priorc1 = 1e-4, priorc2 = 1e-4, priorc12 = 1e-5, 
+                      beta1 = 'beta_1', se1 = 'se_1', beta2 = 'beta_2', se2 = 'se_2') {
+    abf1 <- abf.Wakefield(data[[beta1]], data[[se1]], priorsd1)
+    abf2 <- abf.Wakefield(data[[beta2]], data[[se2]], priorsd2)
+    w <- which(!is.na(abf1) & !is.na(abf2))
+    abf1 <- abf1[w] / (1 + sum(abf1[w]))
+    abf2 <- abf2[w] / (1 + sum(abf2[w]))
+    res <- data.frame(hypothesis = paste0("H", 0:4),
+                      posterior = c((1 - sum(abf1))*(1 - sum(abf2)), 
+                                    priorc1 * sum(abf1)*(1 - sum(abf2)), 
+                                    priorc2 * (1 - sum(abf1))*sum(abf2), 
+                                    priorc1*priorc2 * (sum(abf1)*sum(abf2) - sum(abf1*abf2)), 
+                                    priorc12 * sum(abf1*abf2)),
+                      label = c("No association",
+                                "One variant associated with phenotype 1 only",
+                                "One variant associated with phenotype 2 only",
+                                "Two variants separately associated with phenotypes 1 and 2",
+                                "One variant associated with phenotypes 1 and 2"))
+    res$posterior <- res$posterior/sum(res$posterior)
+    if (is.finite(rounded)) res$posterior = round(res$posterior, rounded)
+    return(res)
+}
