de0af2061cb4609093e1bfc332f5f913b23a2fff
Toby Johnson <Toby.x.Johnson@gsk.com>
2017-03-27 12:11:13 +0100

    Improved numerical stability in coloc.fast and abf.Wakefield; coloc.fast exported

--- R/abf.R
+++ R/abf.R
@@ -2,7 +2,15 @@
 
 ## wakefield ABF inverted to be BF for alternate model relative to null
 ## assumes prior mean is zero
-abf.Wakefield <- function(beta, se, priorsd) return(sqrt(se^2/(se^2 + priorsd^2)) * exp((beta/se)^2/2 * priorsd^2/(se^2+priorsd^2)))
+abf.Wakefield <- function(beta, se, priorsd, log = FALSE) {
+  if (log) {
+    return(log(sqrt(se^2/(se^2 + priorsd^2))) + 
+           (beta/se)^2/2 * priorsd^2/(se^2 + priorsd^2))
+  } else {
+    return(sqrt(se^2/(se^2 + priorsd^2)) * 
+           exp((beta/se)^2/2 * priorsd^2/(se^2 + priorsd^2)))
+  }
+}
 
 ## code used for 2011 AJHG paper used fixed quadrature grid, xx <- seq(from = -0.5, to = 0.5, length.out = 1001)
 
@@ -30,27 +38,45 @@ abf.t <- function(beta, se, priorscale, df = 1, gridrange = 3, griddensity = 20)
   }))
 }
 
+norm1 <- function(x, log = FALSE) {
+  if (all(is.na(x))) return(x)
+  if (log) {
+    x <- x - max(x, na.rm = TRUE)
+    x <- exp(x)    
+  } else {
+    ## This does not work if x contains NaNs or +Infs
+    stopifnot(all(x >= 0, na.rm = TRUE))
+    x <- x / max(x, na.rm = TRUE)
+  }
+  return(x / sum(x, na.rm = TRUE))
+}
+
 ## Colocalisation analysis, implementing method of Giambartolomei et al. 2014
 coloc.fast <- function(data, rounded = 6,
-                      priorsd1 = 1, priorsd2 = 1, priorc1 = 1e-4, priorc2 = 1e-4, priorc12 = 1e-5, 
-                      beta1 = 'beta_1', se1 = 'se_1', beta2 = 'beta_2', se2 = 'se_2') {
-    abf1 <- abf.Wakefield(data[[beta1]], data[[se1]], priorsd1)
-    abf2 <- abf.Wakefield(data[[beta2]], data[[se2]], priorsd2)
-    w <- which(!is.na(abf1) & !is.na(abf2))
-    abf1 <- abf1[w] / (1 + sum(abf1[w]))
-    abf2 <- abf2[w] / (1 + sum(abf2[w]))
-    res <- data.frame(hypothesis = paste0("H", 0:4),
-                      posterior = c((1 - sum(abf1))*(1 - sum(abf2)), 
-                                    priorc1 * sum(abf1)*(1 - sum(abf2)), 
-                                    priorc2 * (1 - sum(abf1))*sum(abf2), 
-                                    priorc1*priorc2 * (sum(abf1)*sum(abf2) - sum(abf1*abf2)), 
-                                    priorc12 * sum(abf1*abf2)),
-                      label = c("No association",
-                                "One variant associated with phenotype 1 only",
-                                "One variant associated with phenotype 2 only",
-                                "Two variants separately associated with phenotypes 1 and 2",
-                                "One variant associated with phenotypes 1 and 2"))
-    res$posterior <- res$posterior/sum(res$posterior)
-    if (is.finite(rounded)) res$posterior = round(res$posterior, rounded)
-    return(res)
+                       priorsd1 = 1, priorsd2 = 1, priorc1 = 1e-4, priorc2 = 1e-4, priorc12 = 1e-5, 
+                       beta1 = 'beta_1', se1 = 'se_1', beta2 = 'beta_2', se2 = 'se_2') {
+  abf1 <- abf.Wakefield(data[[beta1]], data[[se1]], priorsd1, log = TRUE)
+  abf2 <- abf.Wakefield(data[[beta2]], data[[se2]], priorsd2, log = TRUE)
+  w <- which(!is.na(abf1) & !is.na(abf2))
+  nv <- length(w)
+  abf1 <- norm1(c(0, abf1[w]), log = TRUE)
+  abf2 <- norm1(c(0, abf2[w]), log = TRUE)
+  res <- data.frame(hypothesis = paste0("H", 0:4),
+                    label = c("No association",
+                      "One variant associated with phenotype 1 only",
+                      "One variant associated with phenotype 2 only",
+                      "Two variants separately associated with phenotypes 1 and 2",
+                      "One variant associated with phenotypes 1 and 2"),
+                    prior = norm1(c(1, priorc1*nv, priorc2*nv, priorc1*priorc2*nv*(nv - 1), priorc12*nv)),
+                    bf = c(abf1[1]*abf2[1], 
+                      sum(abf1[-1])*abf2[1]/nv, 
+                      abf1[1]*sum(abf2[-1])/nv, 
+                      (sum(abf1[-1])*sum(abf2[-1]) - sum(abf1[-1]*abf2[-1]))/(nv*(nv - 1)), 
+                      sum(abf1[-1]*abf2[-1])/nv))
+  res$bf <- res$bf/res$bf[1]
+  res$posterior <- norm1(res$prior*res$bf)
+  if (is.finite(rounded)) {
+    res$posterior = round(res$posterior, rounded)
+  }
+  return(list(results = res, nvariants = length(w)))
 }
