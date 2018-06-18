39152ac2fc1d96dd803da253f836d9ee235ee39b
Giovanni DallOlio <giovanni.m.dallolio@gsk.com>
2018-06-18 04:22:56 -0400

    DOCS: explaining setup-xyz.R

--- tests/testthat/setup-xyz.R
+++ tests/testthat/setup-xyz.R
@@ -1,3 +1,6 @@
+# This file is executed before all tests start
+# See the file teardown-xyz, which be executed after the last test, and will close these connections
+
 options(gtx.dbConnection = odbcConnect('impaladsn'))
 sqlQuery(getOption('gtx.dbConnection'), 'USE ukbiobank;')
 
