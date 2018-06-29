c2bd1a6ecafcb56873fab313b3897f7a300efd2f
Toby Johnson <Toby.x.Johnson@gsk.com>
2018-06-29 09:35:37 +0100

    FINEMAP: Improved notation and comments in code

--- R/finemap.R
+++ R/finemap.R
@@ -4,40 +4,52 @@
 
 ## Detailed multisignal results are exposed via cleo()
 
-## Trying to use the following notation
-## ppc = posterior probability of causality
-## ccs = credibly causal set
-## _signal = computed uasing single signal assumption
+## Trying to use the following notation in user-facing objects:
+## pp = posterior probability (of causality, for one variant)
+## nullpp = null/emptry model posterior probability
+## cs = credible set (of models, both with variants and empty/null)
+## _signal = computed using single signal assumption
 ## _cleo = computed using CLEO analyses
+##
+## lnabf = ln(ABF), for model with one variant, versus null/empty model
+## Note that lnabf>0 indicates support for model with one variant, unlike original Wakefield definition
+
+## Consistent notation for internal calculations:
+## lnabf = ln(ABF) as above
+## lnabfp = ln(ABF) + ln(prior), with relative prior for variant added, relative to prior=1 for null/empty model
+##    hence lnabfp==0 for the null/empty model
+## ppu = exp(lnabfp - K), posterior probability (unnormalized), where K is a constant within signals
+## maxlnabfp = usual method for choosing K, max over variants *and null*
+## sumppu = denominator for calculating pp, sum over variants *and null*
 
 ## Note, rough exploration with AD meta-analysis of IGAP and UKB results
 ## suggests that, for lnodds units, and with priorsd=1, priorc=1e-05 gives
-## sensible results for nullppc, as a function of lead variant P-value,
+## sensible results for nullpp, as a function of lead variant P-value,
 ## across a range of highly-borderline-non-significant loci
 ## FIXME make a small notebook documenting this
 ## Broader discussion with StatGen, change the default priors for coloc?
 
 fm_signal <- function(data,
-                      priorsd = 1, priorc = 1e-5, ccs_size = 0.95, ccs_only = FALSE) {
+                      priorsd = 1, priorc = 1e-5, cs_size = 0.95, cs_only = FALSE) {
     stopifnot(all(c('beta', 'se') %in% names(data)))
     data$lnabf <- with(data, abf.Wakefield(beta, se, priorsd, log = TRUE))
     ##abf <- norm1(c(0, data$lnabf), log = TRUE) # general decision not to return this intermediate quantity
-    ppc <- norm1(c(0, log(priorc) + data$lnabf), log = TRUE)
+    pp <- norm1(c(0, data$lnabf + log(priorc)), log = TRUE)
     ##abf[is.na(abf)] <- 0.
-    ppc[is.na(ppc)] <- 0. # should be option to leave as NA or set to zero
+    pp[is.na(pp)] <- 0. # should be option to leave as NA or set to zero
     ##data$abf_signal <- abf[-1]
-    data$ppc_signal <- ppc[-1]
-    data$ccs_signal <- credset(ppc, cred = ccs_size)[-1]
-    if (ccs_only) data <- data[which(data$ccs_signal), ]
-    attr(data, 'params_signal') <- list(priorsd = priorsd, priorc = priorc, ccs_size = ccs_size)
+    data$pp_signal <- pp[-1]
+    data$cs_signal <- credset(pp, cred = cs_size)[-1]
+    if (cs_only) data <- data[which(data$cs_signal), ]
+    attr(data, 'params_signal') <- list(priorsd = priorsd, priorc = priorc, cs_size = cs_size)
     ##attr(data, 'nullabf_signal') <- abf[1]
-    attr(data, 'nullppc_signal') <- ppc[1]
+    attr(data, 'nullpp_signal') <- pp[1]
     return(data)
 }
 
-## currently a single function to query all conditional signals that
-## (partly or fully) overlap a query region, and compute causal probs
-## currently, selection by entity is not supported
+## internal function to query all conditional signals that (partly or
+## fully) overlap a query region
+## FIXME: currently, selection by entity is not supported
 fm_cleo.data <- function(analysis,
                          chrom, pos_start, pos_end, pos, 
                          hgncid, ensemblid, rs, surround = 500000,
@@ -75,6 +87,7 @@ fm_cleo.data <- function(analysis,
         xregion$pos_start <- min(ss$pos)
         xregion$pos_end <- max(ss$pos)
     } else {
+        ## FIXME db query with LIMIT 0 is an expensive way to generate a df with 0 rows
         ss <- sqlWrapper(dbc,
                          sprintf('SELECT chrom,pos,ref,alt,signal,beta_cond,se_cond FROM %s.gwas_results_cond LIMIT 0;', 
                                  gtxanalysisdb(analysis)), 
@@ -85,13 +98,16 @@ fm_cleo.data <- function(analysis,
     return(ss)
 }
 
-## currently, selection by entity is not supported
+## internal function to calculate posterior probabilities and credible
+## sets from data returned by fm_cleo.data()
+## FIXME: currently, selection by entity is not supported
 fm_cleo <- function(analysis,
                     chrom, pos_start, pos_end, pos, 
                     hgncid, ensemblid, rs, surround = 500000,
-                    priorsd = 1., priorc = 1e-5, ccs_size = 0.95, ccs_only = FALSE,
+                    priorsd = 1., priorc = 1e-5, cs_size = 0.95, cs_only = FALSE,
                     dbc = getOption("gtx.dbConnection", NULL)) {
-    ## database connection is checked within fm_cleo.data() call
+    
+    ## no database connection check needed, because it happens within fm_cleo.data()
 
     data <- fm_cleo.data(analysis = analysis,
                          chrom = chrom, pos_start = pos_start, pos_end = pos_end, pos = pos,
@@ -105,34 +121,41 @@ fm_cleo <- function(analysis,
         data$lnabfp <- data$lnabf + log(priorc)
         ## rescale so max is 0, over variants *and the null*
         ## including null should not be needed *here*, if CLEO has selected significant signals, unless priorc is *very* small
-        ## FIXME this is a more stable scaling to use in other fm calculations
+        ## FIXME: this is a more stable scaling, propagate its use in other fm calculations
         maxlnabfp <- aggregate(data[ , 'lnabfp', drop = FALSE], by = list(signal = data$signal), FUN = max, na.rm = TRUE)
         maxlnabfp$lnabfp <- pmax(maxlnabfp$lnabfp, 0.)
-        ## compute unnormalized ppc (ppcu)
-        data$ppcu <- exp(data$lnabfp - maxlnabfp$lnabfp[match(data$signal, maxlnabfp$signal)])
+        ## compute unnormalized pp (ppu)
+        data$ppu <- exp(data$lnabfp - maxlnabfp$lnabfp[match(data$signal, maxlnabfp$signal)])
         ## compute sum over variants and nulls within signals, normalize
-        sumppcu <- aggregate(data[ , 'ppcu', drop = FALSE], by = list(signal = data$signal), FUN = sum, na.rm = TRUE)
-        sumppcu$nullppcu <- exp(0. - maxlnabfp$lnabfp)[match(sumppcu$signal, maxlnabfp$signal)]
-        sumppcu$sumppcu <- sumppcu$ppcu + sumppcu$nullppcu
-        sumppcu$nullppc <- sumppcu$nullppcu/sumppcu$sumppcu
-        data$ppc_cleo <- data$ppcu/sumppcu$sumppcu[match(data$signal, sumppcu$signal)]
-        ## clean up data
+        sumppu <- aggregate(data[ , 'ppu', drop = FALSE], by = list(signal = data$signal), FUN = sum, na.rm = TRUE)
+        sumppu$nullppu <- exp(0. - maxlnabfp$lnabfp)[match(sumppu$signal, maxlnabfp$signal)]
+        sumppu$sumppu <- sumppu$ppu + sumppu$nullppu
+        sumppu$nullpp <- sumppu$nullppu/sumppu$sumppu
+        data$pp_cleo <- data$ppu/sumppu$sumppu[match(data$signal, sumppu$signal)]
+        ## remove unwanted columns from data
         data$lnabfp <- NULL
-        data$ppcu <- NULL
-        ## extract null ppcs
-        nullppc <- sumppcu$nullppc 
-        names(nullppc) <- sumppcu$signal
-        ## compute credibly causal sets
-        data$ccs_cleo <- NA
-        for (sig in sumppcu$signal) {
+        data$ppu <- NULL
+        ## extract nullpp for each signal
+        nullpp <- sumppu$nullpp 
+        names(nullpp) <- sumppu$signal
+        ## compute credible sets for each signal
+        ## FIXME: Optimize to remove loop over repeated which (i.e. groupby).  Slightly complicated by
+        ## dependence on nullpp
+        ## Consider adding dummy rows with chrom=pos=ref=alt=beta=se=NA to represent the empty model for each signal
+        ## This would make the normalizing easier
+        data$cs_cleo <- NA
+        for (sig in sumppu$signal) {
             ww <- which(data$signal == sig)
-            data$ccs_cleo[ww] <- gtx:::credset(c(nullppc[match(sig, names(nullppc))], data$ppc[ww]), cred = ccs_size)[-1]
+            data$cs_cleo[ww] <- gtx:::credset(c(nullpp[match(sig, names(nullpp))], data$pp[ww]), cred = cs_size)[-1]
         }
-        ## add as attr()ibutes
-        attr(data, 'params_cleo') <- list(priorsd = priorsd, priorc = priorc, ccs_size = ccs_size)
-        attr(data, 'nullppc_cleo') <- nullppc
-        if (ccs_only) data <- subset(data, ccs_cleo)
+        ## add attr()ibutes
+        attr(data, 'params_cleo') <- list(priorsd = priorsd, priorc = priorc, cs_size = cs_size)
+        attr(data, 'nullpp_cleo') <- nullpp
+
+        ## return either the cs only, or the complete data
+        if (cs_only) return(subset(data, cs_cleo))
         return(data)
+        
     } else {
         ## data have no rows, FIXME add expected columns and attr()ibutes
         return(data)
@@ -142,18 +165,18 @@ fm_cleo <- function(analysis,
 cleo <- function(analysis,
                  chrom, pos_start, pos_end, pos, 
                  hgncid, ensemblid, rs, surround = 500000,
-                 priorsd = 1., priorc = 1e-5, ccs_size = 0.95, 
+                 priorsd = 1., priorc = 1e-5, cs_size = 0.95, 
                  dbc = getOption("gtx.dbConnection", NULL)) {
     ## database connection is checked within fm_cleo.data() call
 
     ## If in future, priorsd and priorc are columns in analyses
     ## *here* is the place we would query and use those values
 
-    ## call fm_cleo with ccs_only = FALSE
+    ## call fm_cleo with cs_only = FALSE
     data <- fm_cleo(analysis = analysis,
                     chrom = chrom, pos_start = pos_start, pos_end = pos_end, pos = pos,
                     hgncid = hgncid, ensemblid = ensemblid, rs = rs, surround = surround,
-                    priorsd = priorsd, priorc = priorc, ccs_size = ccs_size, ccs_only = FALSE,
+                    priorsd = priorsd, priorc = priorc, cs_size = cs_size, cs_only = FALSE,
                     dbc = dbc)
     xregion <- attr(data, 'region')
 
@@ -177,46 +200,47 @@ cleo <- function(analysis,
     ## PCI is probability of causal impact
     datai <- subset(datas, !is.na(impact), drop = FALSE) # FIXME when content of impact changes
     if (nrow(datai) > 0) {
-        pci1 <- aggregate(datai[, 'ppc_cleo', drop = FALSE], 
+        pci1 <- aggregate(datai[, 'pp_cleo', drop = FALSE], 
                          by = list(signal = datai$signal),
                          FUN = sum, na.rm = TRUE)
-        pci1 <- pci1[order(pci1$ppc_cleo, decreasing = TRUE), ]
-        pci <- pci1$ppc_cleo
+        pci1 <- pci1[order(pci1$pp_cleo, decreasing = TRUE), ]
+        pci <- pci1$pp_cleo
         names(pci) <- pci1$signal
-        ## FIXME do setdiff with names(attr(data, 'nullppc_cleo')) to add true zeros
+        ## FIXME do setdiff with names(attr(data, 'nullpp_cleo')) to add true zeros
     } else {
-        pci <- NULL # FIXME should be all zero with signals from names(attr(data, 'nullppc_cleo'))
+        pci <- NULL # FIXME should be all zero with signals from names(attr(data, 'nullpp_cleo'))
     }
 
     ## aggregate over signals
+    ## FIXME, this throws error if data has no rows
     gtxlog('Aggregating over signals')
     t0 <- as.double(Sys.time())
-    dsumppc <- aggregate(data[ , 'ppc_cleo', drop = FALSE], 
+    dsumpp <- aggregate(data[ , 'pp_cleo', drop = FALSE], 
                          by = with(data, list(chrom = chrom, pos = pos, ref = ref, alt = alt)),
                          FUN = sum, na.rm = TRUE)
-    dorccs <- aggregate(data[ , 'ccs_cleo', drop = FALSE], 
+    dorcs <- aggregate(data[ , 'cs_cleo', drop = FALSE], 
                         by = with(data, list(chrom = chrom, pos = pos, ref = ref, alt = alt)),
                         FUN = any)
-    dataa <- merge(merge(dsumppc, dorccs, by = c('chrom', 'pos', 'ref', 'alt'), all = TRUE),
+    dataa <- merge(merge(dsumpp, dorcs, by = c('chrom', 'pos', 'ref', 'alt'), all = TRUE),
                    vep, by = c('chrom', 'pos', 'ref', 'alt'), all.x = TRUE, all.y = FALSE)
     dataa <- dataa[with(dataa, order(chrom, pos, ref, alt)), ]
     t1 <- as.double(Sys.time())
     gtxlog('Aggregated in ', round(t1 - t0, 3), 's.')
 
-    datas = subset(datas, ccs_cleo) # drop to signalwise CCS
+    datas = subset(datas, cs_cleo) # drop to signalwise CS
     
     ##Preserve original attributes, note this list should be introspected not hard coded (FIXME)
-    for (a in c('analysis', 'region', 'params_cleo', 'nullppc_cleo')) {
+    for (a in c('analysis', 'region', 'params_cleo', 'nullpp_cleo')) {
         attr(datai, a) <- attr(data, a)
         attr(dataa, a) <- attr(data, a)
     }
 
-    nullppc <- attr(data, 'nullppc_cleo')
-    nullppc <- nullppc[order(nullppc)]
+    nullpp <- attr(data, 'nullpp_cleo')
+    nullpp <- nullpp[order(nullpp)]
     
-    return(list(nsignals = length(attr(data, 'nullppc_cleo')),
-                nullppc = nullppc,
-                altppc = 1. - nullppc, # need to think of a better name for this 'alt model ppc'
+    return(list(nsignals = length(attr(data, 'nullpp_cleo')),
+                nullpp = nullpp,
+                altpp = 1. - nullpp, # need to think of a better name for this 'alt model ppc'
                 pci = pci, 
                 signals = datas,
                 aggregate = dataa))
