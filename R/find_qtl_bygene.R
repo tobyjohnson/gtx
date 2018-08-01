c176f31e32bea353717a55d10a44be6734243b03
Giovanni DallOlio <giovanni.m.dallolio@gsk.com>
2018-08-01 07:59:30 -0400

    QTL: added pvalue cutoff

--- R/find_qtl_bygene.R
+++ R/find_qtl_bygene.R
@@ -9,7 +9,7 @@ find_qtl_bygene <- function(ensemblid, cutoff=2e-5
 	qtl_analyses_sql = 'SELECT analysis FROM analyses WHERE entity_type NOT IN ("", "NA")'
 	qtl_analyses = sqlWrapper(dbc, qtl_analyses_sql)
 
-	qtls_sql = sprintf("SELECT * FROM gwas_results WHERE entity=%s AND analysis in ('%s') ", ensemblid, paste(qtl_analyses, collapse="','"))
+	qtls_sql = sprintf("SELECT * FROM gwas_results WHERE entity=%s AND analysis in ('%s') AND pval<%s", ensemblid, paste(qtl_analyses, collapse="','"), cutoff)
 	print(qtls_sql)
 
 	qtls = sqlWrapper(dbc, qtls_sql)
