92277683cedee2b71601315e1251f1c9ffb077d4
Giovanni DallOlio <giovanni.m.dallolio@gsk.com>
2018-08-01 07:58:56 -0400

    QTL: stub for a function to get all QTLs for a given gene

---
+++ R/find_qtl_bygene.R
@@ -0,0 +1,18 @@
+#' Given an Ensembl ID, identify all significant QTLs
+#'
+#' 
+find_qtl_bygene <- function(ensemblid, cutoff=2e-5
+			    dbc = getOption("gtx.dbConnection", NULL)) {
+	gtxdbcheck(dbc)
+	# TODO: Could call gtxentity to identify the entity type
+
+	qtl_analyses_sql = 'SELECT analysis FROM analyses WHERE entity_type NOT IN ("", "NA")'
+	qtl_analyses = sqlWrapper(dbc, qtl_analyses_sql)
+
+	qtls_sql = sprintf("SELECT * FROM gwas_results WHERE entity=%s AND analysis in ('%s') ", ensemblid, paste(qtl_analyses, collapse="','"))
+	print(qtls_sql)
+
+	qtls = sqlWrapper(dbc, qtls_sql)
+	qtls
+}
+
