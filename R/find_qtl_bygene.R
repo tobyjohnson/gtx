c1f9ef9b678c8e74e39d37304a83d56a537448d7
Giovanni DallOlio <giovanni.m.dallolio@gsk.com>
2018-08-01 11:22:46 -0400

    QTL: function tested and working on Gtx2-dev environment and notebooks

--- R/find_qtl_bygene.R
+++ R/find_qtl_bygene.R
@@ -1,18 +1,23 @@
 #' Given an Ensembl ID, identify all significant QTLs
 #'
 #' 
-find_qtl_bygene <- function(ensemblid, cutoff=2e-5
-			    dbc = getOption("gtx.dbConnection", NULL)) {
-	gtxdbcheck(dbc)
-	# TODO: Could call gtxentity to identify the entity type
+find_qtl_bygene <- function(ensemblid, cutoff=2e-5,
+        dbc = getOption("gtx.dbConnection", NULL)) {
+    gtxdbcheck(dbc)
 
-	qtl_analyses_sql = 'SELECT analysis FROM analyses WHERE entity_type NOT IN ("", "NA")'
-	qtl_analyses = sqlWrapper(dbc, qtl_analyses_sql)
+    qtl_analyses_sql = 'SELECT analysis FROM analyses WHERE entity_type NOT IN ("", "NA")'
+    qtl_analyses = dbGetQuery(dbc, qtl_analyses_sql)
 
-	qtls_sql = sprintf("SELECT * FROM gwas_results WHERE entity=%s AND analysis in ('%s') AND pval<%s", ensemblid, paste(qtl_analyses, collapse="','"), cutoff)
-	print(qtls_sql)
+    qtls_sql = paste0("SELECT * FROM gwas_results WHERE entity='", 
+                      ensemblid, 
+                      "' AND analysis in ('", 
+                      "", paste(qtl_analyses$analysis, collapse="', '"),
+                      "') AND pval<", cutoff)
+#    print(qtls_sql)
 
-	qtls = sqlWrapper(dbc, qtls_sql)
-	qtls
+    qtls = dbGetQuery(dbc, qtls_sql)
+    qtls
 }
-
+#options(gtx.dbConnection = dbConnect(odbc(), dsn = 'impaladsn'))
+#dbGetQuery(getOption('gtx.dbConnection'), 'USE ukbiobank;')
+#find_qtl_bygene("ENSG00000137033")
