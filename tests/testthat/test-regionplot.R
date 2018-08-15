4b01840434fac0f61a03972bd673d3d3f29c9ffc
Karen King <karen.s.king@gsk.com>
2018-08-14 21:11:12 -0400

    REGIONPLOT: Handle no LD found when selecting index variant by smallest pval (UB-131)

--- tests/testthat/test-regionplot.R
+++ tests/testthat/test-regionplot.R
@@ -5,3 +5,7 @@ test_that("basic regionplot works", {
   
   expect_success(regionplot("gsk_ukb500k_imputed_interim_bolt_f_3571_0_0_f_BIN_Back_pain_for_3_months_vs_no_pain", chrom = '6', pos = 32566577, surround = 5e3, ensemblid = 'ENSG00000013573'))
 })
+
+test_that("UB-131 Handle no LD data when selecting index variant by smallest pval", {
+  regionplot('GSK500kV2b_TA_copdMapLoose_noAsthma_primAndSec_a1', hgncid = 'IL5', case_emac_ge = 25, style = 'ld')
+})
