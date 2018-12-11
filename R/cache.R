656c0cd6aaba460f4c81f465ecab6699c3517759
Toby Johnson <Toby.x.Johnson@gsk.com>
2018-12-11 07:33:47 -0500

    CACHE: Cache analyses_tags

--- R/cache.R
+++ R/cache.R
@@ -10,7 +10,7 @@
 #' 
 #' @export
 gtxcache <- function(cache_analyses = TRUE, 
-                     cache_genes = TRUE, 
+                     cache_genes = TRUE,  
                      disconnect = FALSE, 
                      dbc_from = getOption("gtx.dbConnection", NULL), 
                      dbc_to = getOption('gtx.dbConnection_cache', NULL)) {
@@ -18,16 +18,16 @@ gtxcache <- function(cache_analyses = TRUE,
     ## create connection to in-memory SQLite db where the cache will be kept,
     ## if it hasn't already been created
     if (is.null(dbc_to)) {
-        gtxlog('Creating new in-memory SQLite db')
+        futile.logger::flog.info('Creating new in-memory SQLite db')
         dbc_to <- dbConnect(RSQLite::SQLite(), ':memory:')
         options(gtx.dbConnection_cache = dbc_to)
     } else {
-        gtxlog('Using existing in-memory SQLite db')
+        futile.logger::flog.info('Using existing in-memory SQLite db')
     }
     ## we should check this is a valid dbc
 
     if (disconnect) {
-        gtxlog('Disconnecting from in-memory SQLite db')
+        futile.logger::flog.info('Disconnecting from in-memory SQLite db')
         dbDisconnect(getOption('gtx.dbConnection_cache'))
         options(gtx.dbConnection_cache = NULL, 
                 gtx.dbConnection_cache_analyses = NULL, 
@@ -38,7 +38,7 @@ gtxcache <- function(cache_analyses = TRUE,
     gtxdbcheck(dbc_from) # this is the *source* database that we are caching content from
    
     if (cache_analyses) {
-        gtxlog('Caching TABLE analyses')
+        futile.logger::flog.info('Caching TABLE analyses ...')
         t0 <- as.double(Sys.time())
         DBI::dbWriteTable(getOption('gtx.dbConnection_cache'),
                      'analyses',
@@ -47,15 +47,28 @@ gtxcache <- function(cache_analyses = TRUE,
         DBI::dbExecute(getOption('gtx.dbConnection_cache'),
                   'CREATE INDEX analyses_analysis ON analyses (analysis)') # should be unique
         t1 <- as.double(Sys.time())
-        gtxlog('Cached and INDEXed TABLE analyses with ',
+        futile.logger::flog.info(paste0('Cached and INDEXed TABLE analyses with ',
                     sqlWrapper(getOption('gtx.dbConnection_cache'),
                             'SELECT count(1) AS n FROM analyses')$n,
-                    ' rows in ', round(t1 - t0, 3), 's.')
+                    ' rows in ', round(t1 - t0, 3), 's.'))
+        futile.logger::flog.info('Caching TABLE analyses_tags ...')
+        t0 <- as.double(Sys.time())
+        DBI::dbWriteTable(getOption('gtx.dbConnection_cache'),
+                          'analyses_tags',
+                          sqlWrapper(dbc_from, 'SELECT * FROM analyses_tags;', uniq = FALSE),
+                          overwrite = TRUE)
+        DBI::dbExecute(getOption('gtx.dbConnection_cache'),
+                       'CREATE INDEX analyses_tags_analysis ON analyses_tags (analysis)') # may not be unique
+        t1 <- as.double(Sys.time())
+        futile.logger::flog.info(paste0('Cached and INDEXed TABLE analyses_tags with ',
+                  sqlWrapper(getOption('gtx.dbConnection_cache'),
+                             'SELECT count(1) AS n FROM analyses_tags')$n,
+                  ' rows in ', round(t1 - t0, 3), 's.'))
         options(gtx.dbConnection_cache_analyses = getOption('gtx.dbConnection_cache'))
     }
     
     if (cache_genes) {
-        gtxlog('Caching TABLE genes')
+        futile.logger::flog.info('Caching TABLE genes ...')
         t0 <- as.double(Sys.time())
         DBI::dbWriteTable(getOption('gtx.dbConnection_cache'),
                      'genes',
@@ -64,10 +77,10 @@ gtxcache <- function(cache_analyses = TRUE,
         DBI::dbExecute(getOption('gtx.dbConnection_cache'),
                   'CREATE INDEX genes_chrom_pos_start_pos_end ON genes (chrom, pos_start, pos_end)')
         t1 <- as.double(Sys.time())
-        gtxlog('Cached and INDEXed TABLE genes with ',
+        futile.logger::flog.info(paste0('Cached and INDEXed TABLE genes with ',
                     sqlWrapper(getOption('gtx.dbConnection_cache'),
                             'SELECT count(1) AS n FROM genes')$n,
-                    ' rows in ', round(t1 - t0, 3), 's.')
+                    ' rows in ', round(t1 - t0, 3), 's.'))
         options(gtx.dbConnection_cache_genes = getOption('gtx.dbConnection_cache'))
     }
     
