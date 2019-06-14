343a55fb371d3296b1a99556595df8a1dad4c97b
Adrian Joseph <aj503625@us1salxhpe0017.corpnet2.com>
2019-06-14 03:06:53 -0400

    format test-gwas

--- tests/testthat/test-gwas.R
+++ tests/testthat/test-gwas.R
@@ -2,5 +2,6 @@ context("gwas")
 
 test_that("Unknown error", {
   options('gtx.debug'=TRUE)
-  results <- g1 <- gwas('UKB50k_TA_copdExacerbation_NI_HES_SPIROMETRY', style = 'manhattan', pval_thresh = 5e-06)
+  results <- g1 <- gwas('UKB50k_TA_copdExacerbation_NI_HES_SPIROMETRY',
+                        style = 'manhattan', pval_thresh = 5e-06)
 })
